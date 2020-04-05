60c2bd650aa0b2b9e2dbe9a718d81d8dd14e7952
Martin Doti <mdoti92@gmail.com>
2020-04-05 17:41:42 -0300

    Se agrega clase usuario con sus atributos

---
+++ Usuario.java
@@ -0,0 +1,15 @@
+import java.util.Objects;
+
+/**
+ *
+ * @author TitanDriom
+ */
+public class Usuario {
+    private String nombre;
+    private String cedula;
+    private String contrasenia;
+    private int edad;
+    private int cantidadDeHijos;
+    private int saldo = 0;
+    
+}
